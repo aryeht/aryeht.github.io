bb1774906ba0fb4c253101522e6f399b78e9eafd
Aryeht <aryeh@tikalk.com>
2022-05-03 17:43:22 +0300

    add pyodide nodejs example

--- palisade/pke_node/simple_integer.js
+++ palisade/pke_node/simple_integer.js
@@ -1,5 +1,5 @@
 async function main() {
-  const myCrypto = require('/home/tikal/projects/pyodide/releases/pyodide-build-0.20.0/pyodide/palisade/crypto')
+  const myCrypto = require('../../palisade/crypto')
 
   const createCrypto = () => {
     return new myCrypto();
@@ -10,7 +10,7 @@ async function main() {
   const factory = require('/data1/duality/secureplus-cfw/web/js/lib/palisade_pke')
   const module = await factory()
 
-  let pyodide_pkg = await import("/home/tikal/projects/pyodide/releases/pyodide-build-0.20.0/pyodide/pyodide.js");
+  let pyodide_pkg = await import("../../pyodide.js");
 
   // let pyodide = await pyodide_pkg.loadPyodide({
   //   indexURL: "/home/tikal/projects/pyodide/releases/pyodide-build-0.20.0/pyodide",
